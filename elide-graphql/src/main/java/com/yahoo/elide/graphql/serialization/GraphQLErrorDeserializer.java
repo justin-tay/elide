5343d94d8f3d73abf214761e401d24e9417828ef
Justin Tay <49700559+justin-tay@users.noreply.github.com>
2023-09-05 20:33:51 +0800

    Refactor serializers

--- elide-graphql/src/main/java/com/yahoo/elide/graphql/serialization/GraphQLErrorDeserializer.java
+++ elide-graphql/src/main/java/com/yahoo/elide/graphql/serialization/GraphQLErrorDeserializer.java
@@ -4,13 +4,12 @@
  * See LICENSE file in project root for terms.
  */
 
-package com.yahoo.elide.graphql;
-
-import static graphql.ErrorType.ExecutionAborted;
+package com.yahoo.elide.graphql.serialization;
 
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.deser.std.StdDeserializer;
 
 import graphql.ErrorClassification;
@@ -19,12 +18,15 @@ import graphql.language.SourceLocation;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Deserializes JSON into a GraphQLError.
  */
 public class GraphQLErrorDeserializer extends StdDeserializer<GraphQLError> {
+    private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
 
     /**
      * Constructor.
@@ -40,8 +42,11 @@ public class GraphQLErrorDeserializer extends StdDeserializer<GraphQLError> {
         JsonNode messageNode = root.get("message");
         JsonNode pathNode = root.get("path");
         JsonNode sourceLocations = root.get("locations");
+        JsonNode extensions = root.get("extensions");
+        Map<String, Object> extensionsMap = extensions == null ? Collections.emptyMap()
+                : OBJECT_MAPPER.convertValue(extensions, Map.class);
 
-        GraphQLError error = new GraphQLError() {
+        return new GraphQLError() {
             @Override
             public String toString() {
                 return String.format("{ \"message\": \"%s\", \"locations\": %s, \"path\": %s}",
@@ -59,12 +64,12 @@ public class GraphQLErrorDeserializer extends StdDeserializer<GraphQLError> {
             public List<SourceLocation> getLocations() {
                 if (sourceLocations != null) {
                     List<SourceLocation> result = new ArrayList<>();
-                    sourceLocations.forEach(sourceLocation -> {
+                    sourceLocations.forEach(sourceLocation ->
                         result.add(new SourceLocation(
                                 sourceLocation.get("line").asInt(),
                                 sourceLocation.get("column").asInt()
-                        ));
-                    });
+                        ))
+                    );
 
                     return result;
                 }
@@ -72,23 +77,30 @@ public class GraphQLErrorDeserializer extends StdDeserializer<GraphQLError> {
             }
 
             @Override
+            public Map<String, Object> getExtensions() {
+                return extensionsMap;
+            }
+
+            @Override
             public ErrorClassification getErrorType() {
-                return ExecutionAborted;
+                Object classification = extensionsMap.get("classification");
+                if (classification == null) {
+                    classification = "DataFetchingException";
+                }
+                return ErrorClassification.errorClassification(classification.toString());
             }
 
             @Override
             public List<Object> getPath() {
                 if (pathNode != null) {
                     List<Object> paths = new ArrayList<>();
-                    pathNode.forEach(path -> {
-                        paths.add(path.asText());
-                    });
+                    pathNode.forEach(path ->
+                        paths.add(path.asText())
+                    );
                     return paths;
                 }
                 return null;
             }
         };
-
-        return error;
     }
 }
